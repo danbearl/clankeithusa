944819db3d26967eea3fae900a5f031c6ee3cc6c
Dan Bearl <dan.bearl@gmail.com>
2012-12-21 16:21:14 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clankeithusa::Application.config.secret_token = '8cd802d6e93b3e0ee95e0772f3d3e40b1caf4adcd7713f61d453e86e64a5f59e1f7925d0e52a9b3dee30f73526b06c625ad721ecb75d74ec4c3ace11e8c462ef'
