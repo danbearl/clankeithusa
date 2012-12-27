65e5e6a68433a5bc12953788eebb9eff5b7d634b
Dan Bearl <dan.bearl@gmail.com>
2012-12-27 16:08:50 -0500

    Add section_id column to pages.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121227204539) do
+ActiveRecord::Schema.define(:version => 20121227210621) do
 
   create_table "pages", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20121227204539) do
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "section_id"
   end
 
   create_table "sections", :force => true do |t|
