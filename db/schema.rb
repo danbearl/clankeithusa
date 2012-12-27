19c7b51d74cb305fe0d31828f001f7325e372dee
Dan Bearl <dan.bearl@gmail.com>
2012-12-27 15:47:28 -0500

    Build sections DB table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121227023202) do
+ActiveRecord::Schema.define(:version => 20121227204539) do
 
   create_table "pages", :force => true do |t|
     t.string   "name"
     t.string   "slug"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "sections", :force => true do |t|
+    t.string   "name"
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
