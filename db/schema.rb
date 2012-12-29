41b5ee6590e8625b4a68e850ae4353927ba3a025
Dan Bearl <dan.bearl@gmail.com>
2012-12-28 23:53:18 -0500

    Build images table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121228213551) do
+ActiveRecord::Schema.define(:version => 20121229044609) do
+
+  create_table "images", :force => true do |t|
+    t.string   "name"
+    t.string   "picture"
+    t.integer  "imageable_id"
+    t.string   "imageable_type"
+    t.integer  "priority"
+    t.text     "description"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
   create_table "pages", :force => true do |t|
     t.string   "name"
