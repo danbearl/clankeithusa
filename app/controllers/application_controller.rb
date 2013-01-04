4bf5b487eb09d3b14d94ed5f838e638ceaa066da
Dan Bearl <dan.bearl@gmail.com>
2013-01-04 15:10:18 -0500

    Add user_is_admin? helper method to application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :user_is_admin?
 
   include Authem::ControllerSupport
 
   expose(:pages)
   expose(:sections) do
     pages.where(:parent_id => nil)
+  end
+
+  def user_is_admin?
+    if current_user
+      return current_user.admin
+    else
+      return false
+    end
   end
 end
