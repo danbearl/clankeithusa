5e33e1c32b0facc6708fae66e5f4482b3a33c96d
Dan Bearl <dan.bearl@gmail.com>
2013-01-04 14:31:30 -0500

    Configure authem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  include Authem::ControllerSupport
+
   expose(:pages)
   expose(:sections) do
     pages.where(:parent_id => nil)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :admin, :crypted_password, :email, :name, :salt
+  attr_accessible :admin, :crypted_password, :email, :name, :salt, :password, :password_confirmation
+
+  include Authem::Model
+
 end
