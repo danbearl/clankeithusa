a184be4a2d836518029fe2b18f2528e50ad73232
Dan Bearl <dan.bearl@gmail.com>
2013-01-04 14:38:13 -0500

    Add create to user_sessions_controller

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,12 @@
 class UserSessionsController < ApplicationController
-  def new
+
+  skip_before_filter :require_user, except: :destroy
+
+  def create
+    if sign_in(params[:email], params[:password])
+      redirect_back_or_to(root_path)
+    else
+      render :new
+    end
   end
 end
