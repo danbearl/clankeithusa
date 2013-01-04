8126d4f96e6e35b0c2cbf8afba0ac09e692ca748
Dan Bearl <dan.bearl@gmail.com>
2013-01-04 14:23:01 -0500

    Scope calendar events to allow filtering

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,32 @@
 class EventsController < ApplicationController
-
   expose(:events)
+  expose(:events_scoped) do |events|
+    events = Event.scoped
+    events = events.after(params[:start]) if (params[:start])
+    events = events.before(params[:end]) if (params[:end])
+  end
   expose(:event)
   expose(:new_event, model: Event)
+
+  def index
+
+    if params[:start_date]
+      params[:start] = Event.param_to_date(params[:start_date])
+    end
+
+    if params[:end_date]
+      params[:end] = Event.param_to_date(params[:end_date])
+    end
+
+    if !params[:start]
+      params[:start] = Time.utc(Time.now.year, Time.now.month, 1)
+    end
+
+    if !params[:end]
+      params[:end] = Time.utc(Time.now.year, Time.now.month, 31)
+    end
+
+  end
 
   def create
     if new_event.save
@@ -22,7 +46,7 @@ class EventsController < ApplicationController
 
   def destroy
     event.destroy
-    redirect_to :root, notice: "Event successfully deleted."
+    redirect_to events_path, notice: "Event successfully deleted."
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ActiveRecord::Base
   attr_accessible :description, :ends_at, :starts_at, :title
+
+  scope :before, lambda {|end_time| {conditions: ["ends_at < ?", Event.format_date(end_time)] }}
+  scope :after, lambda {|start_time| {conditions: ["starts_at > ?", Event.format_date(start_time)] }}
+
+  def self.format_date(date_time)
+    Time.at(date_time.to_i).to_formatted_s(:db)
+  end
+
+  def self.param_to_date(params)
+    Time.utc(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+  end
 end
