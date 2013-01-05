3417e96a8f5e453829ab0e17af2a42099e4978d6
Dan Bearl <dan.bearl@gmail.com>
2013-01-05 15:52:48 -0500

    Add logic to blurbs controller.

--- app/controllers/blurbs_controller.rb
+++ app/controllers/blurbs_controller.rb
@@ -1,13 +1,30 @@
 class BlurbsController < ApplicationController
-  def new
+  
+  before_filter :require_admin, only: [:new, :create, :edit, :destroy, :index]
+
+  expose(:blurbs)
+  expose(:blurb)
+  expose(:new_blurb, model: Blurb)
+  expose(:parent) { Page.find(blurb.page_id) }
+
+  def create
+    if new_blurb.save
+      redirect_to slug_path(parent.slug)
+    else
+      render 'new'
+    end
   end
 
-  def edit
+  def update
+    if blurb.save
+      redirect_to slug_path(parent.slug)
+    else
+      render 'edit'
+    end
   end
 
-  def index
-  end
-
-  def show
+  def destroy
+    blurb.destroy
+    redirect_to :root, message: "#{blurb.name} was deleted."
   end
 end
