4dbe9112b02233dda019e1c8162bfef89f6b009f
Dan Bearl <dan.bearl@gmail.com>
2013-01-06 06:25:06 -0500

    Generate announcement model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130105171122) do
+ActiveRecord::Schema.define(:version => 20130106111713) do
+
+  create_table "announcements", :force => true do |t|
+    t.string   "name"
+    t.string   "body"
+    t.boolean  "front_page"
+    t.boolean  "site_wide"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "blurbs", :force => true do |t|
     t.string   "name"
