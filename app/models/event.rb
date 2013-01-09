1c0adbce98978540e024c51d2344beabaa0c28ce
Dan Bearl <dan.bearl@gmail.com>
2013-01-08 21:39:24 -0500

    Add validation error messages for events form.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ActiveRecord::Base
   attr_accessible :description, :ends_at, :starts_at, :title
 
+  validate :must_not_end_before_it_starts
+  validates_presence_of :starts_at, :ends_at
+
   scope :before, lambda {|end_time| {conditions: ["ends_at < ?", Event.format_date(end_time)] }}
   scope :after, lambda {|start_time| {conditions: ["starts_at > ?", Event.format_date(start_time)] }}
 
@@ -10,5 +13,13 @@ class Event < ActiveRecord::Base
 
   def self.param_to_date(params)
     Time.utc(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+  end
+
+  def must_not_end_before_it_starts
+    if ends_at and starts_at
+      if ends_at < starts_at
+        errors.add(:ends_at, "can't end before start date.")
+      end
+    end
   end
 end
