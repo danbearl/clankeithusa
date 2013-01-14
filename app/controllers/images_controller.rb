0829bc9cdc65db36403339636980cc7efd6d3c36
Dan Bearl <dan.bearl@gmail.com>
2013-01-14 16:02:11 -0500

    Fix routing on image#create

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -8,7 +8,11 @@ class ImagesController < ApplicationController
 
   def create
     if image.save
-      redirect_to slug_path(owner.slug), notice: "Image successfully created."
+      if image.page_id != 0 and image.page_id != nil
+        redirect_to slug_path(owner.slug), notice: "Image successfully updated."
+      else
+        redirect_to images_path, notice: "Image succesfully updated."
+      end
     else
       render "new"
     end
