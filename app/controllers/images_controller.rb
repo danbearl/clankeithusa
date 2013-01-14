cae39009c6935429f4dadd6f5ef70156f7541b57
Dan Bearl <dan.bearl@gmail.com>
2013-01-14 15:38:22 -0500

    Add featured checkbox to images forms

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -16,7 +16,11 @@ class ImagesController < ApplicationController
 
   def update
     if image.save
-      redirect_to slug_path(owner.slug), notice: "Image successfully updated."
+      if image.page_id != 0 and image.page_id != nil
+        redirect_to slug_path(owner.slug), notice: "Image successfully updated."
+      else
+        redirect_to images_path, notice: "Image succesfully updated."
+      end
     else
       render "edit"
     end
