3d23fa7ab3e143d3aaa934d4627ba59b8953786d
Dan Bearl <dan.bearl@gmail.com>
2013-01-16 14:42:37 -0500

    Build documents table

--- db/migrate/20130116192443_create_documents.rb
+++ db/migrate/20130116192443_create_documents.rb
@@ -1,6 +1,9 @@
 class CreateDocuments < ActiveRecord::Migration
   def change
     create_table :documents do |t|
+      t.string :name
+      t.text :description
+      t.integer :page_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130114194731) do
+ActiveRecord::Schema.define(:version => 20130116192443) do
 
   create_table "announcements", :force => true do |t|
     t.string   "name"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(:version => 20130114194731) do
     t.boolean  "front_page"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "documents", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "page_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "events", :force => true do |t|
