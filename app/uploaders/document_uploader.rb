c7310922fd0b2e3e4fddcae7aeb758c3dff58972
Dan Bearl <dan.bearl@gmail.com>
2013-01-18 15:57:43 -0500

    Build custom methods in DocumentUploader
    - Convert first page of pdf to jpg for thumbnail

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -10,27 +10,17 @@ class DocumentUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  version :thumb do
-    process :resize_to_fit => [200,200]
-    process :convert => 'jpg'
-    process :paper_shape
-    process :convert => 'jpg'
-    def full_filename (for_file = model.logo.file)
-      super.chomp(File.extname(super)) + '.jpg'
+  def cover
+    manipulate! do |frame, index|
+      frame if index.zero?
     end
   end
 
-  def paper_shape
-    manipulate! do |img|
-      if img.rows*4 != img.columns*3
-        width=img.columns
-        height=img.columns/3*4
-        img.crop!(0,0,width,height,true)
-      else
-        img
-      end
-    end
-        
+  version :thumb do
+    process :cover
+
+    process :resize_to_limit => [200, 200]
+    process :convert => 'jpg'
   end
 
   def extension_white_list
