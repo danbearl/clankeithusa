d493c4aca92f0ab11d50efc58e1e68a6ccd011a5
Dan Bearl <dan.bearl@gmail.com>
2013-01-18 15:55:16 -0500

    Add #create #update and #delete to documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,13 +1,34 @@
 class DocumentsController < ApplicationController
-  def new
+  expose(:documents)
+  expose(:document)
+  expose(:owner) { Page.find(document.page_id) }
+
+  def create
+    if document.save
+      if document.page_id != 0 and document.page_id != nil
+        redirect_to slug_path(owner.slug), notice: "Document successfully updated."
+      else
+        redirect_to documents_path, notice: "Document succesfully updated."
+      end
+    else
+      render "new"
+    end
   end
 
-  def edit
+  def update
+    if document.save
+      if document.page_id != 0 and document.page_id != nil
+        redirect_to slug_path(owner.slug), notice: "Document successfully updated."
+      else
+        redirect_to documents_path, notice: "Document succesfully updated."
+      end
+    else
+      render "edit"
+    end
   end
 
-  def show
-  end
-
-  def index
+  def destroy
+    document.destroy
+    redirect_to documents_path, notice: "Document successfully deleted."
   end
 end
