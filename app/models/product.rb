61992966c183b1655c6db3a5811da7f4fb669146
Dan Bearl <dan.bearl@gmail.com>
2013-02-04 16:16:01 -0500

    Add Product#list_colors & #list_sizes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,19 @@
 class Product < ActiveRecord::Base
   attr_accessible :colors, :description, :image, :name, :price, :sizes
+
+  def list_colors
+    if self.colors == ""
+      return false
+    end
+
+    self.colors.gsub(" ","").split(",")
+  end
+
+  def list_sizes
+    if self.sizes == ""
+      return false
+    end
+
+    self.sizes.gsub(" ","").split(",")
+  end
 end
