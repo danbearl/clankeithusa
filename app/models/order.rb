31adb83beea6f6e72b2c02e74da914db2343bba1
Dan Bearl <dan.bearl@gmail.com>
2013-02-04 22:31:21 -0500

    Add #subtotal, #shipping, and #total to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,41 @@
 class Order < ActiveRecord::Base
   attr_accessible :address_city, :address_state, :address_street_1, :address_street_2, :address_zip, :customer_first_name, :customer_last_name, :email, :notes, :processed, :products, :shipped, :stripe_card_token
+  
+  def subtotal
+    products = Order.unpack_products(self.products)
+    sub = 0
+
+    products.each do |product|
+      next if !product.has_key?(:price)
+
+      sub += product[:price].to_f
+    end
+
+    return sub
+  end
+
+  def shipping
+    sub = subtotal
+    if sub <= 25
+      return 5.95
+    elsif sub <= 50
+      return 7.95
+    elsif sub <= 75
+      return 8.95
+    elsif sub <= 100
+      return 9.95
+    elsif sub <= 150
+      return 12.95
+    elsif sub <= 200
+      return 13.95
+    elsif sub > 200
+      return 14.95
+    end
+  end
+
+  def total
+    return subtotal + shipping
+  end
 
   def self.pack_products(products)
     tmp = []
