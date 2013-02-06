fe700e15fcf7d452d18df0368946650d867ac041
Dan Bearl <dan.bearl@gmail.com>
2013-02-06 15:41:33 -0500

    Build products controller methods
    - Added #create, #update, and #destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,25 @@
 class ProductsController < ApplicationController
-  def new
+  expose(:products)
+  expose(:product)
+
+  def create
+    if product.save
+      redirect_to products_path, notice: "Product successfully created."
+    else
+      render 'new'
+    end
   end
 
-  def edit
+  def update
+    if product.save
+      redirect_to product, notice: "Product successfully updated."
+    else
+      render 'edit'
+    end
   end
 
-  def show
-  end
-
-  def index
+  def destroy
+    product.destroy
+    redirect_to products_path, notice: "Product successfully deleted."
   end
 end
