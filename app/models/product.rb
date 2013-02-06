8140aee68d008111d13bd3db653f1690c6cd9572
Dan Bearl <dan.bearl@gmail.com>
2013-02-06 14:20:26 -0500

    Product mounts ProductImageUploader.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
-  attr_accessible :colors, :description, :image, :name, :price, :sizes
+  attr_accessible :colors, :description, :image, :name, :price, :sizes, :image
+
+  mount_uploader :image, ProductImageUploader
 
   def list_colors
     if self.colors == ""
