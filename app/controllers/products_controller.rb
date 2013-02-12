67a670fbcde89e692733c7f96fcbdf2d2c5dfa50
Daniel Bearl <danielbearl@ubuntu.ubuntu-domain>
2013-02-12 11:53:16 -0500

    Add add_to_cart action to carts controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,4 +22,19 @@ class ProductsController < ApplicationController
     product.destroy
     redirect_to products_path, notice: "Product successfully deleted."
   end
+
+  def add_to_cart
+    unless session[:cart].is_a?(Array) 
+      session[:cart] = [] 
+    end
+
+    cart = {
+      name: params[:name],
+      price: params[:price],
+      quantity: params[:quantity]
+    }
+
+    session[:cart] << cart
+    redirect_to products_path, notice: "Added to cart."
+  end
 end
