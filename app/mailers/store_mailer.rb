40549e20929401c195f8b0194ceee490441619c9
Daniel Bearl <danielbearl@ubuntu.ubuntu-domain>
2013-02-18 12:00:28 -0500

    Add order_confirmation to StoreMailer

--- app/mailers/store_mailer.rb
+++ app/mailers/store_mailer.rb
@@ -1,3 +1,9 @@
 class StoreMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "dan.bearl@gmail.com"
+
+  def order_confirmation(order)
+    @order = order
+
+    mail(to: order.email, subject: "Clan Keith, USA Order Confirmation")
+  end
 end
