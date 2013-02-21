83f6c977f9d217d39c838406bc48ec992d88f026
Dan Bearl <dan.bearl@gmail.com>
2013-02-21 12:26:07 -0500

    Remove stripe_card_token column from orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130213153733) do
+ActiveRecord::Schema.define(:version => 20130221154534) do
 
   create_table "announcements", :force => true do |t|
     t.string   "name"
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(:version => 20130213153733) do
     t.boolean  "shipped"
     t.text     "notes"
     t.text     "products"
-    t.string   "stripe_card_token"
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
     t.string   "stripe_payment_token"
