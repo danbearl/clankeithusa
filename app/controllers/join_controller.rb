ba3c06ba73876641547e699fa1d8f9998601021b
Daniel Bearl <danielbearl@ubuntu.ubuntu-domain>
2013-02-23 13:16:18 -0500

    Refactor membership application

---
+++ app/controllers/join_controller.rb
@@ -0,0 +1,18 @@
+class JoinController < ApplicationController
+
+  def new
+    @applicant = Applicant.new
+  end
+
+  def create
+    @applicant = Applicant.new(params[:applicant])
+
+    if @applicant.valid?
+      StoreMailer.new_applicant(@applicant).deliver
+
+      redirect_to :root, notice: "Thank you for your applicantion. We will review it promptly and contact you with a response."
+    else
+      render 'new'
+    end
+  end
+end
