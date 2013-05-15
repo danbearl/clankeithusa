88358e560d151c0b52ac5417207a5ac8a2508c33
Dan Bearl <dan.bearl@gmail.com>
2013-05-15 15:15:19 -0400

    Nesting blurb_associations fields in pages form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,17 +3,24 @@ class PagesController < ApplicationController
   before_filter :require_admin, only: [:new, :create, :edit, :destroy, :index]
 
   expose(:page, finder: :find_by_slug, finder_parameter: :slug)
-  expose(:new_page, model: Page)
   expose(:images) { page.images }
-  expose(:blurbs) { page.blurbs }
+  expose(:blurb_associations) { page.blurb_associations }
   expose(:sub_pages) {page.sub_pages.reorder(:priority) }
 
+  def new
+    page.blurb_associations.build
+  end
+
   def create
-    if new_page.save
-      redirect_to slug_path(new_page.slug), notice: "Page successfully created."
+    if page.save
+      redirect_to slug_path(page.slug), notice: "Page successfully created."
     else
       render 'new'
     end
+  end
+
+  def edit
+    page.blurb_associations.build
   end
 
   def update
