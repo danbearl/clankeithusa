f5dd28c370c0482db8b23e5f92dcb4b7e9f3969c
Dan Bearl <dan.bearl@gmail.com>
2013-05-20 19:58:33 -0400

    Add heirarchical decorations to pages#index

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,10 +1,10 @@
 module PagesHelper
-  def index_rows(pages)
+  def index_rows(pages, depth = 0)
     markup = ""
     pages.each do |page|
       markup << %Q{
         <tr>
-          <td>#{link_to page.id, slug_path(page.slug) }</td>
+          <td>#{depth_count(depth)}#{link_to page.id, slug_path(page.slug) }</td>
           <td>#{ page.name }</td>
           <td>#{ page.updated_at.to_s(:us) }</td>
           <td>#{page.priority}</td>
@@ -17,11 +17,21 @@ module PagesHelper
       }
             
       if page.sub_pages.count > 0
-        markup << index_rows(page.sub_pages)
+        markup << index_rows(page.sub_pages, depth + 1)
       end
     end
     
     return markup
 
   end
+
+  def depth_count(depth)
+    str = ""
+    depth.times do
+      str << ">"
+    end
+
+    return str
+  end
+
 end
