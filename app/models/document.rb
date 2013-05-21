38cfba7cbbc7cdc6e98573703d6ce94803714530
Dan Bearl <dan.bearl@gmail.com>
2013-05-20 21:55:14 -0400

    Refactor document forms

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,4 +4,12 @@ class Document < ActiveRecord::Base
   belongs_to :page
 
   mount_uploader :file, DocumentUploader
+
+  def owner
+    unless self.page_id.nil_or_zero?
+      Page.find(self.page_id).name
+    else
+      "None"
+    end
+  end
 end
